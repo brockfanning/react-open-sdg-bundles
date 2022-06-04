38de51d009e9898f003727d98d619725e7e6363b
Brock Fanning <brockfanning@gmail.com>
2022-06-04 12:41:47 -0400

    Trying with open sdg site config schema

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: {
